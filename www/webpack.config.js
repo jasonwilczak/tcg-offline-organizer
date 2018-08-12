17f8feb68ce3288ebc668cc91925eb71601da80a
EC2 Default User <ec2-user@ip-172-31-61-53.ec2.internal>
2018-08-12 08:45:13 +0000

    initial react app up and running

---
+++ www/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        options: {
+          presets: ["env", "react"]
+        }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
